9412dfa18db0f667ce7f3a3f890011bcb3ea7ca6
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2012-09-22 03:24:13 -0700

    ordering issue on postgre

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @new_artifacts = Artifact.order("created_at DESC").limit(3)
-    @popular_artifacts = Artifact.order("comment_count DESC").limit(3)
-    @latest_bid_artifacts = Artifact.order("bid_at DESC").limit(3)
+    if Rails.env.development?
+      @new_artifacts = Artifact.order("created_at DESC").limit(3)
+      @popular_artifacts = Artifact.order("comment_count DESC").limit(3)
+      @latest_bid_artifacts = Artifact.order("bid_at DESC").limit(3)
+    else
+      @new_artifacts = Artifact.order("created_at DESC NULLS LAST").limit(3)
+      @popular_artifacts = Artifact.order("comment_count DESC NULLS LAST").limit(3)
+      @latest_bid_artifacts = Artifact.order("bid_at DESC NULLS LAST").limit(3)
+    end
+
   end
 end
