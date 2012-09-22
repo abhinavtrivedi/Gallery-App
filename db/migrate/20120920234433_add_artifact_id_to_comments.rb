8311ec7f2103d116bbebc27e42744db33779af3d
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2012-09-22 02:30:41 -0700

    changing foreign keys to integer (error in heroku)

--- db/migrate/20120920234433_add_artifact_id_to_comments.rb
+++ db/migrate/20120920234433_add_artifact_id_to_comments.rb
@@ -1,6 +1,12 @@
 class AddArtifactIdToComments < ActiveRecord::Migration
-  def change
-    add_column :comments, :artifact_id, :string
+  def up
+    add_column :comments, :artifact_id, :integer
     add_index :comments, :artifact_id
   end
+
+  def down
+    remove_column :comments, :artifact_id
+    remove_index :comments, :artifact_id
+  end
+
 end
